09d58979a73360a482fe463390dbbf05197c35e9
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-15 09:47:11 +0100

    update app.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,19 +9,19 @@
 import DefaultBar from "@/layouts/default/AppBar.vue";
 import DefaultView from "@/layouts/default/View.vue";
 import { onMounted } from "vue";
-import { useConnectionStore, useSafeStore } from "./store";
+import { useConnectionStore /* useSafeStore  */ } from "./store";
 import { connection } from "./utils";
 
 const { isConnected, wallet } = useConnectionStore();
-const safeStore = useSafeStore();
+// const safeStore = useSafeStore();
 
 onMounted(async () => {
   if (isConnected) {
     await connection.connect(wallet);
   }
 
-  if (safeStore.activeSafeId) {
-    safeStore.fetchActiveSafe();
-  }
+  // if (safeStore.activeSafeId) {
+  //   safeStore.fetchActiveSafe();
+  // }
 });
 </script>
