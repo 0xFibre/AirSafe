972913ab7ae7f2102cdf09e6f35bc2d266063b58
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-17 13:41:00 +0100

    settings page update

--- app/src/views/Settings.vue
+++ app/src/views/Settings.vue
@@ -1,10 +1,16 @@
 <template>
   <v-row>
     <v-col cols="12" md="6">
-      <v-card flat class="mb-3">
+      <v-card flat class="mb-5">
+        <v-card-text>
+          <h6 class="text-h6 font-weight-bold fonted">Change safe name</h6>
+        </v-card-text>
+
+        <v-divider />
+
         <v-card-text>
           <p class="text-body-2 fonted mb-3">Safe Name</p>
-          <v-text-field variant="outlined" density="comfortable" />
+          <v-text-field color="primary" variant="outlined" density="compact" />
 
           <div class="d-flex">
             <v-spacer />
@@ -21,12 +27,29 @@
         <v-divider />
 
         <v-card-text>
-          <p class="text-body-2 fonted mb-3">Threshold</p>
-          <v-select variant="outlined" density="comfortable" />
+          <p class="text-body-2 fonted mb-3">
+            The minimum number of owners required to approve any transaction
+          </p>
+
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                type="number"
+                color="primary"
+                variant="outlined"
+                density="compact"
+                class="py-1"
+              >
+                <template v-slot:append>
+                  <div>out of 1 owners</div>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
 
           <div class="d-flex">
             <v-spacer />
-            <v-btn flat color="primary">Update</v-btn>
+            <v-btn flat color="primary">Change</v-btn>
           </div>
         </v-card-text>
       </v-card>
