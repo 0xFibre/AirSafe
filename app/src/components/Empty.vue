fa7f34f0ab057c972f533226ed582d58dd6a3645
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-17 20:58:30 +0100

    update empty responses

--- app/src/components/Empty.vue
+++ app/src/components/Empty.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="text-center my-3">
+  <div class="text-center text-disabled my-5">
     <p>{{ msg }}</p>
+    <!-- <v-img src="/assets/empty.svg" height="100" /> -->
   </div>
 </template>
 
