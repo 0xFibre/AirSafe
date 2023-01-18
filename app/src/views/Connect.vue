02e5672defea13b5e762ef7f9ca0380739c7db71
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-18 09:53:22 +0100

    connect wallet page update

--- app/src/views/Connect.vue
+++ app/src/views/Connect.vue
@@ -4,21 +4,19 @@
       <h6 class="my-3 font-weight-bold text-h6 fonted">Connect your wallet</h6>
 
       <v-list>
-        <v-list-item
-          v-for="(wallet, i) in wallets"
-          :key="i"
-          :value="wallet"
-          class="my-1"
-          @click="connect(wallet.name)"
-        >
-          <template v-slot:prepend>
-            <v-avatar size="30">
-              <v-img :src="wallet.icon" :alt="wallet.name" />
-            </v-avatar>
-          </template>
+        <template v-for="(wallet, i) in wallets" :key="i">
+          <v-divider v-if="i != 0" />
 
-          <v-list-item-title v-text="wallet.name" />
-        </v-list-item>
+          <v-list-item @click="connect(wallet.name)">
+            <template v-slot:prepend>
+              <v-avatar size="30">
+                <v-img :src="wallet.icon" :alt="wallet.name" />
+              </v-avatar>
+            </template>
+
+            <v-list-item-title>{{ wallet.name }}</v-list-item-title>
+          </v-list-item>
+        </template>
       </v-list>
     </v-col>
   </v-row>
@@ -27,11 +25,14 @@
 <script lang="ts" setup>
 import { connection } from "@/utils";
 import { useConnectionStore } from "@/store";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const connectionStore = useConnectionStore();
 const { wallets } = connection;
 
 async function connect(name: string) {
   await connectionStore.establishConnection(name);
+  router.push({ name: "Safes" });
 }
 </script>
