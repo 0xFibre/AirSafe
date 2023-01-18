6d413828fa304fa9f323a348ab78616d1903c639
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-18 10:17:30 +0100

    connect wallet page update

--- app/src/views/Connect.vue
+++ app/src/views/Connect.vue
@@ -1,20 +1,28 @@
 <template>
   <v-row class="mt-10">
     <v-col md="6" sm="8" cols="12" class="mx-auto">
-      <h6 class="my-3 font-weight-bold text-h6 fonted">Connect your wallet</h6>
+      <div class="my-5">
+        <h6 class="font-weight-bold text-h6 fonted">Connect your wallet</h6>
+        <p>
+          Choose any of your installed wallets to sign in to
+          <strong>{{ config.appName }}</strong>
+        </p>
+      </div>
 
-      <v-list>
+      <v-list class="py-0">
         <template v-for="(wallet, i) in wallets" :key="i">
           <v-divider v-if="i != 0" />
 
-          <v-list-item @click="connect(wallet.name)">
-            <template v-slot:prepend>
+          <v-list-item class="pa-4" @click="connect(wallet.name)">
+            <template v-slot:append>
               <v-avatar size="30">
                 <v-img :src="wallet.icon" :alt="wallet.name" />
               </v-avatar>
             </template>
 
-            <v-list-item-title>{{ wallet.name }}</v-list-item-title>
+            <template v-slot:prepend>
+              <v-list-item-title>{{ wallet.name }}</v-list-item-title>
+            </template>
           </v-list-item>
         </template>
       </v-list>
@@ -26,6 +34,7 @@
 import { connection } from "@/utils";
 import { useConnectionStore } from "@/store";
 import { useRouter } from "vue-router";
+import { config } from "@/config";
 
 const router = useRouter();
 const connectionStore = useConnectionStore();
