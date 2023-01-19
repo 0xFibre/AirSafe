aefdf01acf4d6c32103bce5a65faa8d2acc83a21
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-19 16:02:17 +0100

    add vue toast

--- app/src/plugins/index.ts
+++ app/src/plugins/index.ts
@@ -3,7 +3,8 @@ import type { App } from "vue";
 import { vuetify } from "./vuetify";
 import { pinia } from "../store";
 import { router } from "../router";
+import { Toast } from "./toast";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia);
+  app.use(vuetify).use(router).use(pinia).use(Toast);
 }
