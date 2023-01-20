6dad3e39c3c6df247f8febd13dd65583355aaa48
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-20 21:43:46 +0100

    update try

--- landing/src/components/Try.vue
+++ landing/src/components/Try.vue
@@ -1,11 +1,24 @@
 <template>
   <v-sheet class="my-10 text-center">
     <v-container>
-      <h4 class="text-h4 font-weight-bold mb-5">Try AirSafe now</h4>
-      <p class=""></p>
+      <div class="mb-5">
+        <h4 class="text-h4 font-weight-bold mb-3">Try AirSafe now!</h4>
+        <p>
+          Try out AirSafe on Sui devnet, and get ready for the
+          <strong>#Suinami</strong> now!
+        </p>
+      </div>
 
       <div class="mt-8">
-        <v-btn flat color="primary">Launch App</v-btn>
+        <v-btn
+          flat
+          color="primary"
+          append-icon="mdi-arrow-right"
+          target="_blank"
+          href="https://app.airsafe.io"
+        >
+          Launch App
+        </v-btn>
       </div>
     </v-container>
   </v-sheet>
